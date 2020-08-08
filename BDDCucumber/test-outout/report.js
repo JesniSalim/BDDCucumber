10104586f62b23fc1e916e69d118c46e463989cd
Jesni <Jesni@LAPTOP-UI4SAG9M>
2020-08-08 19:16:23 +0530

    Datadriven

--- BDDCucumber/test-outout/report.js
+++ BDDCucumber/test-outout/report.js
@@ -1,89 +1,218 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/Jesni/git/BDDCucumber/BDDCucumber/src/main/java/feature/login.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/Jesni/git/BDDCucumber/BDDCucumber/src/main/java/feature/dataDriven.feature");
 formatter.feature({
   "line": 1,
-  "name": "Login",
+  "name": "login DataDriven",
   "description": "",
-  "id": "login",
+  "id": "login-datadriven",
   "keyword": "Feature"
 });
-formatter.scenario({
+formatter.scenarioOutline({
   "line": 3,
-  "name": "Verify Login Page with valid email id and password",
+  "name": "verify login functionality",
   "description": "",
-  "id": "login;verify-login-page-with-valid-email-id-and-password",
-  "type": "scenario",
-  "keyword": "Scenario"
+  "id": "login-datadriven;verify-login-functionality",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
 });
 formatter.step({
   "line": 5,
-  "name": "I should navigate to Home Page",
+  "name": "I have navigate to application",
   "keyword": "Given "
 });
 formatter.step({
   "line": 6,
-  "name": "I should click on Login button",
+  "name": "I enter valid username \"\u003cuname\u003e\"",
   "keyword": "Then "
 });
 formatter.step({
   "line": 7,
-  "name": "I should verify the page title",
+  "name": "I should enter valid password \"\u003cpassword\u003e\"",
   "keyword": "And "
 });
 formatter.step({
   "line": 8,
-  "name": "I should enter email id and password",
+  "name": "I should click on submit button",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 10,
+  "name": "",
+  "description": "",
+  "id": "login-datadriven;verify-login-functionality;",
+  "rows": [
+    {
+      "cells": [
+        "uname",
+        "password"
+      ],
+      "line": 12,
+      "id": "login-datadriven;verify-login-functionality;;1"
+    },
+    {
+      "cells": [
+        "jesni",
+        "jesni123"
+      ],
+      "line": 13,
+      "id": "login-datadriven;verify-login-functionality;;2"
+    },
+    {
+      "cells": [
+        "anu",
+        "anu123"
+      ],
+      "line": 14,
+      "id": "login-datadriven;verify-login-functionality;;3"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 13,
+  "name": "verify login functionality",
+  "description": "",
+  "id": "login-datadriven;verify-login-functionality;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "I have navigate to application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter valid username \"jesni\"",
+  "matchedColumns": [
+    0
+  ],
   "keyword": "Then "
 });
 formatter.step({
-  "line": 9,
-  "name": "I should click on login button",
+  "line": 7,
+  "name": "I should enter valid password \"jesni123\"",
+  "matchedColumns": [
+    1
+  ],
   "keyword": "And "
 });
 formatter.step({
-  "line": 10,
-  "name": "user should navigate to Homepage",
+  "line": 8,
+  "name": "I should click on submit button",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "LoginStepDefinition.i_should_navigate_to_home_page()"
+  "location": "DataDrivenStepDefinition.i_have_navigate_to_application()"
 });
 formatter.result({
-  "duration": 14188831400,
+  "duration": 29631530000,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStepDefinition.i_should_click_on_login_button1()"
+  "arguments": [
+    {
+      "val": "jesni",
+      "offset": 24
+    }
+  ],
+  "location": "DataDrivenStepDefinition.i_enter_valid_username(String)"
 });
 formatter.result({
-  "duration": 1216392800,
+  "duration": 1099851800,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStepDefinition.i_should_verify_the_page_title()"
+  "arguments": [
+    {
+      "val": "jesni123",
+      "offset": 31
+    }
+  ],
+  "location": "DataDrivenStepDefinition.i_should_enter_valid_password_something(String)"
 });
 formatter.result({
-  "duration": 59524600,
+  "duration": 160783500,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStepDefinition.i_should_enter_email_id_and_password()"
+  "location": "DataDrivenStepDefinition.i_should_click_on_submit_button()"
 });
 formatter.result({
-  "duration": 419094100,
+  "duration": 1450495300,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "verify login functionality",
+  "description": "",
+  "id": "login-datadriven;verify-login-functionality;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "I have navigate to application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter valid username \"anu\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 7,
+  "name": "I should enter valid password \"anu123\"",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I should click on submit button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "DataDrivenStepDefinition.i_have_navigate_to_application()"
+});
+formatter.result({
+  "duration": 43481767000,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStepDefinition.i_should_click_on_login_button()"
+  "arguments": [
+    {
+      "val": "anu",
+      "offset": 24
+    }
+  ],
+  "location": "DataDrivenStepDefinition.i_enter_valid_username(String)"
 });
 formatter.result({
-  "duration": 1315174500,
+  "duration": 605940800,
   "status": "passed"
 });
 formatter.match({
-  "location": "LoginStepDefinition.user_should_navigate_to_Homepage()"
+  "arguments": [
+    {
+      "val": "anu123",
+      "offset": 31
+    }
+  ],
+  "location": "DataDrivenStepDefinition.i_should_enter_valid_password_something(String)"
 });
 formatter.result({
-  "duration": 446455900,
+  "duration": 193409100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "DataDrivenStepDefinition.i_should_click_on_submit_button()"
+});
+formatter.result({
+  "duration": 4410428300,
   "status": "passed"
 });
 });
